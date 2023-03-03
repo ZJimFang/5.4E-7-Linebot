36d31262fef0c0ddf2f1f2a823d66a728b735be2
zjimf <jmjmjim1101@gmail.com>
2023-03-03 15:32:27 +0800

    reserve process

--- action.js
+++ action.js
@@ -1,14 +1,30 @@
+const stringToDetail = {
+  A: ["10", 0],
+  B: ["10", 1],
+  C: ["11", 0],
+  D: ["11", 1],
+  E: ["12", 0],
+  F: ["12", 1],
+  G: ["13", 0],
+  H: ["13", 1],
+  I: ["14", 0],
+  J: ["14", 1],
+  K: ["15", 0],
+  L: ["15", 1],
+  M: ["16", 0],
+  N: ["16", 1],
+};
+
 const buildJson = require("./functions/buildJson");
 const admin = require("firebase-admin");
-let flexMessageTemplate = require("./template/flexMsgTemplate.json");
 admin.initializeApp({
   credential: admin.credential.cert(require("./admin.json")),
 });
+const db = admin.firestore();
 
 //concurrent problem
 let dateOrder = 0;
-module.exports.reserve = async () => {
-  const db = admin.firestore();
+module.exports.reserve = async (flexMessageTemplate) => {
   // console.log(dataIndex);
   for (let month = 4; month <= 5; month++) {
     const beginDate = month === 4 ? 10 : 12;
@@ -39,6 +55,17 @@ module.exports.reserve = async () => {
               alphabeticalOrder++;
             }
           });
+          flexMessageTemplate.contents.contents[dateOrder].header.contents.push(
+            {
+              type: "text",
+              text: `如需預約請輸入以下格式\n月/日/時段\n舉例：4/10/A`,
+              margin: "8px",
+              weight: "regular",
+              style: "normal",
+              size: "19px",
+              wrap: true,
+            }
+          );
           console.log("------------------");
         });
       dateOrder++;
@@ -46,4 +73,47 @@ module.exports.reserve = async () => {
   }
   dateOrder = 0;
   return flexMessageTemplate;
+};
+
+module.exports.isReserved = async (request, userID) => {
+  const schedule = request.split("/");
+  let reply = {
+    type: "text",
+    text: "success",
+  };
+  schedule[2] = stringToDetail[schedule[2]];
+  const month = schedule[0];
+  const date = schedule[1];
+  const hour = schedule[2][0];
+  const time = schedule[2][1];
+
+  console.log(month, date, hour, time);
+
+  await db
+    .collection("Booking-Time")
+    .doc(month)
+    .collection(date)
+    .doc(hour)
+    .get()
+    .then((doc) => {
+      const timeIsReserved = doc.data().period[time];
+      if (timeIsReserved) {
+        reply.text = "該時間已被預約";
+      } else {
+        const userIsReserved = false;
+        if (userIsReserved) {
+          reply.text = "每個帳號只能預約一次";
+        } else {
+          reply.text = "請輸入email";
+          db.collection("userInfo")
+            .doc(userID)
+            .set({
+              email: "",
+              reservedTime: [month, date, hour, time],
+            });
+        }
+      }
+    });
+
+  return reply;
 };
--- index.js
+++ index.js
@@ -3,7 +3,6 @@ require("dotenv").config();
 const line = require("@line/bot-sdk");
 const express = require("express");
 const action = require("./action");
-
 const config = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
   channelSecret: process.env.CHANNEL_SECRET,
@@ -12,6 +11,8 @@ const config = {
 const client = new line.Client(config);
 
 const app = express();
+
+let flexMessageTemplate = require("./template/flexMsgTemplate.json");
 
 app.post("/webhook", line.middleware(config), (req, res) => {
   Promise.all(req.body.events.map(handleEvent))
@@ -23,26 +24,25 @@ app.post("/webhook", line.middleware(config), (req, res) => {
 
 // event handler
 async function handleEvent(event) {
+  const userID = event.source.userId;
+
   if (event.type !== "message" || event.message.type !== "text") {
     return Promise.resolve(null);
   }
 
   let reply;
-  switch (event.message.text) {
-    case "預約時間":
-      const reserveReply = await action.reserve();
-      reply = reserveReply;
-      break;
-    case "查詢預約":
-      break;
-    case "刪除預約":
-      break;
+  let request = event.message.text;
 
-    default:
-      break;
-  }
+  const regex = /^[45]\/1[0-5]\/[A-N]$/;
 
-  if (reply) {
+  console.log(regex.test(request));
+
+  if (request === "預約時間") {
+    reply = await action.reserve(flexMessageTemplate);
+    await client.replyMessage(event.replyToken, reply);
+  } else if (request === "查詢/刪除預約") {
+  } else if (regex.test(request)) {
+    reply = await action.isReserved(request, userID);
     await client.replyMessage(event.replyToken, reply);
   }
 }
